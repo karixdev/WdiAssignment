177225118622ad96e00b337f32981f6d3056a21c
karixdev <30869888+karixdev@users.noreply.github.com>
2023-01-26 20:25:19 +0100

    feat: generate plots

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import numpy as np
+from model import LinearRegression
+from optimizer import Optimizer
+import matplotlib.pyplot as plt
 
 np.random.seed(0)
-X = np.random.rand(2, 1)
+X = 3 * np.random.rand(100, 1)
 
 np.random.seed(1)
-y = np.random.randn(2, 1) + 4 + 3 * X
+y = np.random.randn(100, 1) + 4 + 3 * X
 
-print(X)
-print(y)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.scatter(X, y)
+plt.show()
+
+model = LinearRegression()
+optimizer = Optimizer(1000, 0.1)
+
+model.fit(X, y, optimizer)
+
+X_new = np.array([[0], [3]])
+y_predicted = model.predict(X_new)
+
+plt.scatter(X, y)
+plt.plot(X_new, y_predicted, 'r', label="Prosta regresji")
+plt.legend(loc="upper left")
+plt.show()
